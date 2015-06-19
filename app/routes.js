a5fc291f7a9ab1884c2dca198604396c306c07eb
Teddy <Teddy@mail.com>
2015-06-19 10:58:26 +1000

    error working

--- app/routes.js
+++ app/routes.js
@@ -1,33 +1,38 @@
 var _= require('underscore');
 //var validator = require('./bower_components/validator-js/validator.js');
-var validator = require('validator');
+
 
 module.exports = function(app) {
     app.post('/api/filter', function(req, res) {
 
         console.log('inside service call');
+
+        res.setHeader('Content-Type', 'application/json')
+        res.write('you posted:\n')
+        res.end(JSON.stringify(req.body, null, 2))
+
         //valid
-        if (validator.isJSON(req.body.payload)) {
-            console.log(req.body);
-
-            var resp = _.filter(_.where(req.body.payload, {drm: true}), function (item) {
-                return item.episodeCount > 0
-            });
-
-            var newArray = [];
-            resp.forEach(function (item) {
-                var newItem = _.pick(item, 'image', 'slug', 'title');
-                newItem.image = _.propertyOf(newItem.image)('showImage');
-                newArray.push(newItem);
-            })
-
-            res.send(newArray);
-        }
-        else
-        {
-            //invalid json
-            res.send('no good man');
-        }
+        //if (validator.isJSON(JSON.stringify(req.body.payload))) {
+        //    console.log(req.body);
+        //
+        //    var resp = _.filter(_.where(req.body.payload, {drm: true}), function (item) {
+        //        return item.episodeCount > 0
+        //    });
+        //
+        //    var newArray = [];
+        //    resp.forEach(function (item) {
+        //        var newItem = _.pick(item, 'image', 'slug', 'title');
+        //        newItem.image = _.propertyOf(newItem.image)('showImage');
+        //        newArray.push(newItem);
+        //    })
+        //
+        //    res.send(newArray);
+        //}
+        //else
+        //{
+        //    //invalid json
+        //    res.send('{"error": "Could not decode request: JSON parsing failed"}');
+        //}
     });
 
 	// application -------------------------------------------------------------
--- server.js
+++ server.js
@@ -2,22 +2,30 @@
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var port  	 = process.env.PORT || 8080; 				// set the port
-
-
+var validator = require('validator');
 
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({'extended':'false'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
+app.use(methodOverride());
+app.use(clientErrorHandler);
 
-//app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
+///app.use(bodyParser.json());
 
 // routes ======================================================================
 require('./app/routes.js')(app);
 
-
 // listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
+
+function clientErrorHandler(err, req, res, next) {
+    if (!validator.isJSON(JSON.stringify(req.body.payload))) {
+        res.status(500).send({ error: 'Something blew up!' });
+    } else {
+        next(err);
+    }
+}
 
